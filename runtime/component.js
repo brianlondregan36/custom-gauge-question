c89f3867411080e76f485c34406b1486d4b1c57c
Brian Londregan <brian.londregan@gmail.com>
2020-07-07 16:16:39 -0400

    changes to the way we capture design time settings and use them in the run time model

--- runtime/component.js
+++ runtime/component.js
@@ -2,13 +2,15 @@
 register(function (question, customQuestionSettings, questionViewSettings) {
 
   var gauge = {
-    q: currentQuestion,
-    qid: currentQuestion.id,
-    qSel: "#" + currentQuestion.id,
+    q: question,
+    qid: question.id,
+    qSel: "#" + question.id,
     Render: function() {
       $('<div class="cf-question__text">' + this.q.text + '</div>').appendTo(this.qSel);
       $('<div class="cf-question__instruction">' + this.q.instruction + '</div>').appendTo(this.qSel);
       $('<div class="cf-question__content" style="min-height:200px"><div class="bl-question--gauge"><div id="semi1_' + this.qid + '" class="bl-question--gauge__semicircle bl-question--gauge__semicircle--in-motion-1"></div><div id="semi2_' + this.qid + '" class="bl-question--gauge__semicircle bl-question--gauge__semicircle--in-motion-2"></div><div id="semi3_' + this.qid + '" class="bl-question--gauge__semicircle bl-question--gauge__semicircle--fixed"></div><div class="bl-question--gauge__inner"><p id="indicator_' + this.qid + '"></p></div></div></div>').appendTo(this.qSel);
+      var userSelColor = "#" + customQuestionSettings.color;
+      $('.bl-question--gauge__semicircle--in-motion-1, .bl-question--gauge__semicircle--in-motion-2').css('background-color', userSelColor);
     },
     onGaugeChange: function(answer) {
       this.q.setValue(answer);
@@ -21,14 +23,14 @@ register(function (question, customQuestionSettings, questionViewSettings) {
       var r1 = this._GetRotation(one);
       var r2 = this._GetRotation(two);
       if( r1 != 180 ) {
-        r1 = parseInt(r1) + 36;
+        r1 = parseInt(r1) + this._GetIncrementAmount;
         var percent = this._ComputePercent(r1);
         r1 = "rotate(" + r1 + "deg)";
         one.style.transform = r1;
         two.style.transform = r1;
       }
       else if( r1 == 180 && r2 != 360 ) {
-        r2 = parseInt(r2) + 36;
+        r2 = parseInt(r2) + this._GetIncrementAmount;
         var percent = this._ComputePercent(r2);
         r2 = "rotate(" + r2 + "deg)";
         two.style.transform = r2;
@@ -52,14 +54,14 @@ register(function (question, customQuestionSettings, questionViewSettings) {
         fixed.style.zIndex = "3";
       }
       if( r2 != 0 && r2 <= 180) {
-        r2 = parseInt(r2) - 36;
+        r2 = parseInt(r2) - this._GetIncrementAmount;
         var percent = this._ComputePercent(r2);
         r2 = "rotate(" + r2 + "deg)";
         one.style.transform = r2;
         two.style.transform = r2;
       }
       else if( r2 > 180 ) {
-        r2 = parseInt(r2) - 36;
+        r2 = parseInt(r2) - this._GetIncrementAmount;
         var percent = this._ComputePercent(r2);
         r2 = "rotate(" + r2 + "deg)";
         two.style.transform = r2;
@@ -75,6 +77,9 @@ register(function (question, customQuestionSettings, questionViewSettings) {
     },
     _ComputePercent: function(rot) {
       return Math.round(rot / 360 * 100);
+    },
+    _GetIncrementAmount: function() {
+      return Math.round(360/parseInt(customQuestionSettings.increments));
     }
   }
 
